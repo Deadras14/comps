dd8564ba4ee76a76a25662802d9809a8da2292e6
Ralph Emerson Almarez <ralphalmarez29@gmail.com>
2021-06-11 16:14:47 +0800

    Navigating with Router Link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ElementsModule } from './elements/elements.module';
+import { CollectionsModule } from './collections/collections.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+  imports: [ 
+    BrowserModule, 
+    AppRoutingModule, 
+    ElementsModule, 
+    CollectionsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
