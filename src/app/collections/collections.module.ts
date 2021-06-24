3eb8542bc1aed750971747fc2aeaa19f71a0b3f4
Ralph Emerson Almarez <ralphalmarez29@gmail.com>
2021-06-24 12:21:56 +0800

    Exercise Solution

--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -7,6 +7,7 @@ import { TableComponent } from './table/table.component';
 import { BiographyComponent } from './biography/biography.component';
 import { CompaniesComponent } from './companies/companies.component';
 import { PartnersComponent } from './partners/partners.component';
+import { TabsComponent } from './tabs/tabs.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { PartnersComponent } from './partners/partners.component';
     TableComponent,
     BiographyComponent,
     CompaniesComponent,
-    PartnersComponent
+    PartnersComponent,
+    TabsComponent
   ],
   imports: [
     CommonModule,
