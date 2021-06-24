635c2d05d63577b386cfc0f9f7d0ea861c6f8c08
ralph29 <ralphalmarez29@gmail.com>
2021-06-24 17:37:38 +0800

    Displaying a List of Items

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -4,11 +4,13 @@ import { SharedModule } from '../shared/shared.module';
 import { StatisticsComponent } from './statistics/statistics.component';
 import { ViewsHomeComponent } from './views-home/views-home.component';
 import { ViewsRoutingModule } from './views-routing.module';
+import { ItemListComponent } from './item-list/item-list.component';
 
 @NgModule({
   declarations: [
     ViewsHomeComponent,
-    StatisticsComponent
+    StatisticsComponent,
+    ItemListComponent
   ],
   imports: [
     CommonModule,
